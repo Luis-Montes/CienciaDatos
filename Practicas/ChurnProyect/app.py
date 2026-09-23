08f280011b47502e5db23db7f2e73627e489efb3
Luis-Montes <mecsi_montes@hotmail.com>
2026-09-23 01:10:17 -0600

    Agregue la matriz de rendimiento del modelo de prediccion

--- Practicas/ChurnProyect/app.py
+++ Practicas/ChurnProyect/app.py
@@ -4,23 +4,31 @@ import streamlit as st
 # Configuración de la página web
 st.set_page_config(
     page_title="Dashboard de Churn - Predicción de Abandono",
-    page_icon="📊",
+    page_icon="*",
     layout="wide",
 )
 
-st.title("📊 Dashboard Inteligente de Retención de Clientes (Churn)")
+st.title("Dashboard Inteligente de Retención de Clientes (Churn)")
 st.markdown(
     "Aplicación conectada a SQL Server para predecir el riesgo de fuga de clientes mediante Machine Learning."
 )
 
 # Cargamos los datos y el modelo desde el backend
 with st.spinner("Conectando a SQL Server y entrenando el modelo..."):
-    df_clientes, modelo, scaler = be.cargar_y_entrenar_modelo()
+    (
+        df_clientes, 
+        modelo, 
+        scaler, 
+        fig,
+        accuracy,
+        precision,
+        recall
+    )= be.cargar_y_entrenar_modelo()
 
 # ==========================================
 # 1. MÉTRICAS PRINCIPALES (KPIs)
 # ==========================================
-st.subheader("📈 Resumen General")
+st.subheader("Resumen General")
 
 # Simulamos las predicciones para todo el dataset actual
 X_data = df_clientes[["tiempo_en_tienda", "cantidad_compras", "devoluciones"]]
@@ -45,10 +53,32 @@ col3.metric("Tasa de Churn Global", f"{porcentaje_riesgo}%")
 
 divider = st.divider()
 
+st.subheader("Métricas de Rendimiento del Modelo (Evaluación)")
+
+m1, m2, m3 = st.columns(3)
+m1.metric("Exactitud (Accuracy)", f"{accuracy}%")
+m2.metric("Precisión (Precision)", f"{precision}%")
+m3.metric("Sensibilidad (Recall)", f"{recall}%")
+
+# Sección de la Matriz de Confusión al lado de una explicación
+col_m1, col_m2 = st.columns([1, 1])
+with col_m1:
+  st.markdown("""
+        ### ¿Cómo interpretar estas métricas?
+        * **Exactitud:** Porcentaje global de aciertos del modelo.
+        * **Precisión:** De las alertas de fuga enviadas, cuántas fueron reales.
+        * **Sensibilidad:** Qué tantos clientes en riesgo real fuimos capaces de atrapar.
+        * **Algoritmo utilizado:** Regresión Logística.
+        * **Validación:** División 80/20 de los datos históricos.
+        * **Métrica clave:** Capacidad de identificar correctamente a los clientes con alto riesgo de fuga para evitar pérdidas económicas en la tienda.
+    """)
+with col_m2:
+  st.pyplot(fig)
+
 # ==========================================
 # 2. TABLA INTERACTIVA DE CLIENTES
 # ==========================================
-st.subheader("📋 Listado de Clientes y Probabilidad de Fuga")
+st.subheader("Listado de Clientes y Probabilidad de Fuga")
 
 # Filtro interactivo en la barra lateral o en pantalla
 filtro_riesgo = st.checkbox("Mostrar únicamente clientes en alto riesgo (> 50%)")
@@ -58,12 +88,12 @@ if filtro_riesgo:
 else:
     df_mostrar = df_clientes
 
-st.dataframe(df_mostrar, use_container_width=True)
+st.dataframe(df_mostrar, width="stretch")
 
 # ==========================================
 # 3. SIMULADOR DE RIESGO EN VIVO
 # ==========================================
-st.subheader("🔍 Simulador de Riesgo para un Cliente Nuevo")
+st.subheader("Simulador de Riesgo para un Cliente Nuevo")
 
 col_a, col_b, col_c = st.columns(3)
 with col_a:
@@ -72,7 +102,7 @@ with col_a:
     )
 with col_b:
     sim_compras = st.number_input(
-        "Cantidad de Compras", min_value=0, max_value=50, value=1
+        "Cantidad de Compras", min_value=0, max_value=100, value=1
     )
 with col_c:
     sim_devoluciones = st.number_input(
@@ -91,9 +121,9 @@ if st.button("Calcular Riesgo de Abandono"):
 
     if pred_resultado == 1:
         st.error(
-            f"🚨 **Alerta Roja:** Este cliente tiene un **{prob_resultado:.2f}%** de probabilidad de abandono."
+            f"**Alerta Roja:** Este cliente tiene un **{prob_resultado:.2f}%** de probabilidad de abandono."
         )
     else:
         st.success(
-            f"✅ **Cliente Seguro:** Este cliente está retenido. Probabilidad de fuga de solo **{prob_resultado:.2f}%**."
+            f"**Cliente Seguro:** Este cliente está retenido. Probabilidad de fuga de solo **{prob_resultado:.2f}%**."
         )
--- Practicas/ChurnProyect/backend.py
+++ Practicas/ChurnProyect/backend.py
@@ -1,41 +1,78 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from conexion import engine, query
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    precision_score,
+    recall_score
+)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from conexion import engine, query
 
 
 def cargar_y_entrenar_modelo():
   # 1. Extraer datos de SQL Server
-  df_clientes = pd.read_sql(query, engine) # Leemos la conexion y ejecutamos la consulta(desde conexion.py) y la convertimos en un DataFrame de pandas
+  # Leemos la conexion y ejecutamos la consulta(desde conexion.py) y la convertimos en un DataFrame de pandas
+  df_clientes = pd.read_sql(query, engine)
   print("--- DATOS EXTRAÍDOS DE SQL SERVER ---")
   print(df_clientes.head())
 
-  # 2. Revisar los clientes que tengan más de 2 devoluciones o menos de 10 minutos en la tienda y marcarlos como churn (1) o no churn (0)
-  df_clientes["churn"] = np.where( # Se crea la columna churn y agreegamos un condicinal de numpy para marcar a los clientes que cumplen con la condición de riesgo de fuga
-      (df_clientes["devoluciones"] >= 2) | (df_clientes["tiempo_en_tienda"] < 10), # Condición: si el cliente tiene 2 o más devoluciones o ha pasado menos de 10 minutos en la tienda
-      1, # valores asignado si cumple la condición (churn)
+  # 2. Definir la variable objetivo (churn)
+  # Se crea la columna churn y agreegamos un condicinal de numpy para marcar a los clientes que cumplen con la condición de riesgo de fuga
+  df_clientes["churn"] = np.where(
+      # Condición: si el cliente tiene 2 o más devoluciones o ha pasado menos de 10 minutos en la tienda
+      (df_clientes["devoluciones"] >= 2) | (df_clientes["tiempo_en_tienda"] < 10),
+      1,
       0,
-  )
+  ) # Condición: si el cliente tiene 2 o más devoluciones o ha pasado menos de 10 minutos en la tienda
 
-  x = df_clientes[["tiempo_en_tienda", "cantidad_compras", "devoluciones"]] # Separamos las variables predictoras (X) de la variable objetivo (y)
-  y = df_clientes["churn"] # Guardamos la variable objetivo (y) que indica si el cliente es churn o no
+  # Separamos las variables predictoras (X) de la variable objetivo (y)
+  x = df_clientes[["tiempo_en_tienda", "cantidad_compras", "devoluciones"]]
+  # Guardamos la variable objetivo (y) que indica si el cliente es churn o no
+  y = df_clientes["churn"]
 
   # 3. Dividir datos y entrenar
-  X_train, X_test, y_train, y_test = train_test_split( # Separamos los datos en conjunto de entrenamiento y prueba
-      x, y, test_size=0.2, random_state=42 # Separamos el 20% de los datos para prueba y el 80% para entrenamiento, con una semilla aleatoria para reproducibilidad
+  X_train, X_test, y_train, y_test = train_test_split(
+      x, y, test_size=0.2, random_state=42
   )
 
-  scaler = StandardScaler() # Creamos un objeto StandardScaler para normalizar los datos
-  x_train_scaled = scaler.fit_transform(X_train) # Ajustamos el scaler a los datos de entrenamiento y transformamos los datos de entrenamiento
-  X_test_scaled = scaler.transform(X_test) # Transformamos los datos de prueba usando el mismo scaler ajustado a los datos de entrenamiento
+  scaler = StandardScaler()
+  x_train_scaled = scaler.fit_transform(X_train)
+  X_test_scaled = scaler.transform(X_test)
 
-  modelo_churn = LogisticRegression() # Creamos un objeto LogisticRegression para entrenar el modelo de regresión logística
-  modelo_churn.fit(x_train_scaled, y_train) # Entrenamos el modelo con los datos de entrenamiento escalados y la variable objetivo
+  modelo_churn = LogisticRegression()
+  modelo_churn.fit(x_train_scaled, y_train) # Aqui se aplica la Regresión Logística y la Función Sigmoide
 
-  # Devolvemos lo necesario para la interfaz de Streamlit
-  return df_clientes, modelo_churn, scaler
+  # 4. Predicciones y calculo de metricas 
+  y_pred = modelo_churn.predict(X_test_scaled) # Aqui tambien se usan
+  matriz = confusion_matrix(y_test, y_pred)
+
+  accuracy = round(accuracy_score(y_test, y_pred) * 100, 2)
+  precision = round(
+    precision_score(y_test, y_pred, zero_division=0) * 100, 2
+  )
+  recall = round(recall_score(y_test, y_pred, zero_division=0) * 100, 2)
+
+  # 5. Matriz de confusión para evaluar el rendimiento
+  fig, ax = plt.subplots(figsize=(5, 4))
+  sns.heatmap(
+      matriz,
+      annot=True,
+      fmt="d",
+      cmap="Blues",
+      xticklabels=["Retenido(0)", "Abandono(1)"],
+      yticklabels=["Retenido(0)", "Abandono(1)"],
+      ax=ax,
+  )
+  ax.set_title("Matriz de Confusión")
+  ax.set_xlabel("Predicción")
+  ax.set_ylabel("Realidad")
+  plt.tight_layout()
+
+  # Devolvemos todo para la interfaz de Streamlit
+  return df_clientes, modelo_churn, scaler, fig, accuracy, precision, recall
